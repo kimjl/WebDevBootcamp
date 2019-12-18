a4c8057bc9d7c3680319474d6fda00ba95ebac3d
kimjl <jamesleekim90@gmail.com>
2019-12-18 18:27:56 -0500

    Intro to javascript

---
+++ 7 - Introduction to JavaScript/stalkerscript.js
@@ -0,0 +1,10 @@
+var firstName = prompt('What is your first name?');
+var lastName = prompt('What is your last name?');
+var age = prompt('How old are you?');
+
+alert('Nice to meet you ' + firstName + ' ' + lastName);
+alert('You are ' + age + ' years old')
+
+console.log('Nice to meet you ' + firstName + ' ' + lastName);
+console.log('You are ' + age + ' years old')
+
