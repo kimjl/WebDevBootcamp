9bedbc6cfa3dadb29ba713a73ca58dc7a2cf1517
kimjl <jamesleekim90@gmail.com>
2019-12-20 14:18:14 -0500

    dom manipulation

---
+++ 12 - DOM Manipulation/selectors.js
@@ -0,0 +1,11 @@
+var tag = document.getElementById('highlight');
+// console.log(tag);
+
+var tags = document.getElementsByClassName('bolded');
+// console.log(tags[0]);
+
+var tags =  document.getElementsByTagName('li');
+
+var tag = document.querySelector('#highlight');
+var tag = document.querySelector('li');
+var tag = document.querySelector('.bolded');
